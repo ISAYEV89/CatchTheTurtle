b5792e4c41f87cc5d8982466cd7a4fca3dd6b382
PROSYS\elchin.i <elchin.i@prosys.az>
2023-06-08 17:18:24 +0400

    click turtle

--- main.py
+++ main.py
@@ -12,24 +12,34 @@ tur.shape('turtle')
 tur.color("green")
 tur.penup()
 
+
+count = 0
+
+timer_text = turtle.Turtle('circle', visible=False)
+timer_text.penup()
+# timer_text.clear()
+timer_text.write('Score: 0', move=False, font=("Courier", 30))
+# timer_text.setpos(-250,250)
+timer_text.goto(-75, 270)
+def clickTurtle(x,y):
+    global count
+    count = count + 1
+    timer_text.clear()
+    timer_text.goto(-75, 270)
+    timer_text.write(f'Score: {count}', font=("Courier", 16))
+
+
+
+tur.onclick(clickTurtle)
+
+
 # turtle random gelmesi
-for i in range(10):
+for i in range(2):
     tur.hideturtle()
     tur.goto(randint(-250, 250), randint(-200, 200))  # deqiqleshdirilmeli
     time.sleep(0.5)
     tur.showturtle()
-    time.sleep(1)
-
-
-
-
-# timer_text.clear()
-# timer_text.write(i, font=("Courier", 30))
-
-
-
-timer_text = turtle.Turtle('circle', visible=False)
-timer_text.penup()
+    time.sleep(5)
 
 
 
